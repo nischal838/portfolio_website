7b3fe75c3ce8b0291b33a6fe027a50ba294211dc
Nischal Rana <rananischal2011@yahoo.com>
2021-08-29 18:43:31 -0500

    Navbar Components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <h1>Hello World!</h1>
+    <Navbar />
   );
 }
 
